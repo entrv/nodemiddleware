4cc60156a42d878cb904ff77b4a53ecd9fe28938
entrvn7 <entrvn7@gmail.com>
2019-03-29 22:58:03 +0900

    second

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(logger)
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
 	res.send('hellow')
 })
