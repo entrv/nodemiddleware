64d2264102ed51e6fcb88895413d30b9d816a3af
entrvn7 <entrvn7@gmail.com>
2019-04-01 01:29:55 +0900

    helmet add

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const logger = require('./logger')
 const express = require('express')
-
+const helmet = require('helmet')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger)
 app.use(express.static('public'))
-
+app.use(helmet())
+app.use(morgan('tiny'))
 app.get('/', (req, res) => {
 	res.send('hellow')
 })
